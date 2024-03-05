62777c43a33105bcd67413e34f159ee5337d2599
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-03-05 21:14:22 +0300

    first commit

---
+++ src/main/java/com/cengizhanyavuz/wordvault/WordVaultApplication.java
@@ -0,0 +1,13 @@
+package com.cengizhanyavuz.wordvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cengizhanyavuz/wordvault/WordVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cengizhanyavuz.wordvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
