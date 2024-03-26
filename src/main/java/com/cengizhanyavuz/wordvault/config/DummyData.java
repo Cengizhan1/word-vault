2dfa572a191dd75104718713a15a5458ac86913f
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-03-26 03:17:23 +0300

    kelimedb added and DummyData class updated

--- src/main/java/com/cengizhanyavuz/wordvault/config/DummyData.java
+++ src/main/java/com/cengizhanyavuz/wordvault/config/DummyData.java
@@ -3,6 +3,10 @@ package com.cengizhanyavuz.wordvault.config;
 import com.cengizhanyavuz.wordvault.model.Word;
 import com.cengizhanyavuz.wordvault.repository.WordRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +14,9 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @Configuration
 @Slf4j
@@ -22,19 +29,34 @@ public class DummyData {
         this.wordRepository = wordRepository;
     }
 
-    public void dummyWords() {
-        String[] turkishWords = {"Merhaba", "Dünya", "Kitap", "Bilgisayar", "Öğrenmek", "Deneme", "Yazılım", "Geliştirici", "Araba", "Proje"};
-        String[] englishTranslations = {"Hello", "World", "Book", "Computer", "Learn", "Trial", "Software", "Developer", "Car", "Project"};
-
+    public void dummyWords() throws IOException {
+        FileInputStream excelFile = new FileInputStream(new File("src\\main\\resources\\static\\kelimedb.xlsx"));
+        Workbook workbook = new XSSFWorkbook(excelFile);
+        Sheet sheet = workbook.getSheetAt(0);
         List<Word> words = new ArrayList<>();
-        for (int i = 0; i < turkishWords.length; i++) {
+
+        for (Row row : sheet) {
+            if (row.getRowNum() == 0) continue;
+            String column1 = row.getCell(1).getStringCellValue();
+            String column2 = row.getCell(2).getStringCellValue();
+
+            if (column1.contains(",")) {
+                column1 = column1.split(",")[0];
+            }
+            if (column2.contains(",")) {
+                column2 = column2.split(",")[0];
+            }
+
             Word word = new Word();
-            word.setTr(turkishWords[i]);
-            word.setEn(englishTranslations[i]);
+            word.setTr(column2);
+            word.setEn(column1);
             words.add(word);
         }
         wordRepository.saveAll(words);
+        workbook.close();
+        excelFile.close();
     }
+
 
     @Bean
     public CommandLineRunner commandLineRunnerMethod() {
